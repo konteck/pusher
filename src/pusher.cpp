158ae1d21033ff07d45cd6cd8eb2a89886a44140
Alex Movsisyan <dev@movsisyan.com>
2013-02-15 17:51:04 +0400

    Add job queue processing

--- src/pusher.cpp
+++ src/pusher.cpp
@@ -3,6 +3,7 @@
 #include "zmq.hpp"
 #include "picojson.h"
 #include "web++.hpp"
+#include "queue++.h"
 
 #include "gcm.hpp"
 //#include "apns.hpp"
@@ -39,139 +40,6 @@ void SaveToMongo(DBClientConnection* con, string db, string collection, const st
     con->insert(db + "." + collection, b);
 }
 
-/*
-void* doWork(void* ctx) {
-    DBClientConnection mongo_conn;
-    mongo_conn.connect(mongo_host + ":" + mongo_port);
-    
-    zmq::context_t* context = reinterpret_cast<zmq::context_t*>(ctx);
-    zmq::socket_t socket(*context, ZMQ_REP);
-    socket.connect("inproc://workers");
-    
-    zmq_pollitem_t items[1] = { { socket, 0, ZMQ_POLLIN, 0 } };
-    
-    while(true) {
-        stringstream log;
-        object response;
-        double start;
-        
-        try {
-            if(zmq_poll(items, 1, -1) < 1) {
-                cout << "Terminating worker" << endl;
-                break;
-            }
-            
-            zmq::message_t request;
-            socket.recv(&request);
-            
-            start = timer();
-            
-            char* json_str = (char*) request.data();
-            
-            value push;
-            std::string err;
-            parse(push, json_str, json_str + strlen(json_str), &err);
-            
-            if (!err.empty()) {
-                throw string("Request JSON Parse Error: " + err + " " + json_str);
-            }
-            
-            object JSON = push.get<object>();
-            bool status = true;
-            
-            if(JSON["gcm"].is<object>()) {
-                string api_key = CONFIG["gcm"].get<object>()["api_key"].to_str();
-                
-                vector<value> pDevices = JSON["gcm"].get<object>()["devices"].get<array>();
-                vector<string> devices;
-                
-                for(int i = 0; i < pDevices.size(); i++) {
-                    devices.push_back(pDevices[i].to_str());
-                }
-                
-                stringstream data;
-                data << JSON["gcm"].get<object>()["data"];
-                
-                if(!Pusher::gcm_send(api_key, devices, data.str())) {
-                    status = false;
-                }
-                
-                SaveToMongo(&mongo_conn, mongo_db, mongo_collection, data.str());
-            }
-            
-            // Response back
-            response.insert(std::make_pair ("status", * new value(status ? "success" : "error")));
-            
-            // Log
-            time_t now = time(0);
-            tm *ltm = std::localtime(&now);
-            
-            log << "[" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << " " << ltm->tm_mday << "/" << ltm->tm_mon << "] \t"
-            << (status ? "success" : "error") << " " << request.size() << "b "
-            << " \t" << timer(start) << "s";
-        } catch(string e) {
-            response.insert(std::make_pair ("status", * new value("error")));
-            response.insert(std::make_pair ("message", * new value(e)));
-            
-            // Log
-            time_t now = time(0);
-            tm *ltm = std::localtime(&now);
-            
-            log << "[" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << " " << ltm->tm_mday << "/" << ltm->tm_mon << "] \t"
-            << e
-            << " \t" << timer(start) << "s";
-        } catch(const zmq::error_t& e) {
-            response.insert(std::make_pair ("status", * new value("error")));
-            response.insert(std::make_pair ("message", * new value(e.what())));
-            
-            // Log
-            time_t now = time(0);
-            tm *ltm = std::localtime(&now);
-            
-            log << "[" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << " " << ltm->tm_mday << "/" << ltm->tm_mon << "] \t"
-            << "ZeroMQ Worker: " << e.what()
-            << " \t" << timer(start) << "s";
-        } catch(const mongo::DBException &e) {
-            response.insert(std::make_pair ("status", * new value("error")));
-            response.insert(std::make_pair ("message", * new value(e.what())));
-            
-            // Log
-            time_t now = time(0);
-            tm *ltm = std::localtime(&now);
-            
-            log << "[" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << " " << ltm->tm_mday << "/" << ltm->tm_mon << "] \t"
-            << "MongoDB: " << e.what()
-            << " \t" << timer(start) << "s";
-        } catch(exception& e) {
-            response.insert(std::make_pair ("status", * new value("error")));
-            response.insert(std::make_pair ("message", * new value(e.what())));
-            
-            // Log
-            time_t now = time(0);
-            tm *ltm = std::localtime(&now);
-            
-            log << "[" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << " " << ltm->tm_mday << "/" << ltm->tm_mon << "] \t"
-            << "Fatal error: " << e.what()
-            << " \t" << timer(start) << "s";
-        }
-        
-        value resp(response);
-        string resp_text = resp.serialize();
-        zmq::message_t reply(resp_text.size());
-        memcpy (reply.data(), resp_text.c_str(), resp_text.size());
-        //socket.send(reply);
-        
-        cout << log.str() << endl;
-        
-        LOGS.push_back(log.str());
-    }
-    
-    zmq_close(socket);
-    
-    return NULL;
-}
-*/
-
 void* ThreadWorker(void* args) {
     stringstream log;
     object response;
@@ -185,13 +53,13 @@ void* ThreadWorker(void* args) {
         
         char* json_str = (char*)args;
         
-//        sleep(2);
+        sleep(1);
 //        
 //        cout << json_str << endl;
 //
         
-        //cout << getpid() << " " << strlen(json_str) << endl;
-        //return NULL;
+        cout << " " << json_str << endl;
+        return NULL;
         
         value push;
         std::string err;
@@ -335,6 +203,9 @@ void* run(void* arg) {
         socket.bind(uri.c_str());
         mongo_conn->connect(mongo_host + ":" + mongo_port);
         
+        QPP:QPP::Queue jobs(20);
+        jobs.start_nonblocking();
+        
         /////////-----
         //DBClientConnection mongo_conn;
         //mongo_conn.connect(mongo_host + ":" + mongo_port);
@@ -346,7 +217,9 @@ void* run(void* arg) {
                 void *copy = operator new(request.size());
                 memcpy(copy, data, request.size());
                 
-                pthread_create(&workers, NULL, &ThreadWorker, copy);
+                jobs.add_job(&ThreadWorker, copy);
+                
+                //pthread_create(&workers, NULL, &ThreadWorker, copy);
             }
             
             //usleep(10 * 1000);
@@ -379,10 +252,18 @@ void web_interface(WPP::Request* req, WPP::Response* res) {
 */
 
 int main(int argc, const char* argv[]) {
-//    pthread_t main;
-
-//    int rc = pthread_create (&main, NULL, &run, NULL);
-//    assert (rc == 0);
+//    QPP:QPP::Queue jobs(3);
+//    jobs.start_nonblocking();
+//    
+//    for(int i = 0; i < 10; i++) {
+//        jobs.add_job(&wrk, &i);
+//    }
+//    
+//    sleep(20);
+//        
+//    cout << "END" << endl;
+        
+    //cout << "ENDX" << endl;
     
     run(NULL);
 
---
+++ src/queue++.h
@@ -0,0 +1,99 @@
+//
+//  queue++.h
+//  Pusher
+//
+//  Created by Alex Movsisyan.
+//  Copyright (c) 2013 Alex Movsisyan. All rights reserved.
+//
+
+#import <queue>
+
+using namespace std;
+
+namespace QPP {
+    pthread_mutex_t lock;
+    
+    struct Job {
+        void* (*callback)(void*);
+        void* args;
+    };
+    
+    class Queue {
+    public:
+        Queue(int workers_count);
+        void add_job(void* (*callback)(void*));
+        void add_job(void* (*callback)(void*),void*);
+        void start();
+        void start_nonblocking();
+    private:
+        static void* run_loop(void*);
+        int workers_count;
+        deque<Job> jobs_queue;
+    };
+    
+    void* Queue::run_loop(void* context){
+        Queue* cls = (Queue*)context;
+                
+        //cout << cls->workers_count << endl;
+        
+        int active_threads = 0;
+        
+        pthread_t workers[cls->workers_count];
+        
+        while(true) { // !cls->jobs_queue.empty() || active_threads > 0
+            active_threads = 0;
+            
+            for(int i = 0; i < cls->workers_count; i++) {
+                if(pthread_kill(workers[i], 0) != 0) {
+                    pthread_mutex_lock(&lock);
+                    if(cls->jobs_queue.size() > 0) {
+                        Job job = cls->jobs_queue.front();
+                        
+                        assert (pthread_create (&workers[i], NULL, job.callback, job.args) == 0);
+                        cls->jobs_queue.pop_front();
+                    }
+                    pthread_mutex_unlock(&lock);
+                } else { // still running
+                    active_threads++;
+                    
+                    //cout << "Active thread" << endl;
+                }
+            }
+            
+            usleep(500);
+        }
+        
+        return NULL;
+    }
+    
+    Queue::Queue(int workers_count){
+        this->workers_count = workers_count;
+        
+        if (pthread_mutex_init(&lock, NULL) != 0) {
+            cout << "Mutex init failed" << endl;
+        }
+    }
+    
+    void Queue::add_job(void* (*callback)(void*)){
+        this->add_job(callback, NULL);
+        
+    }
+    
+    void Queue::add_job(void* (*callback)(void*), void* args){
+        pthread_mutex_lock(&lock);
+        this->jobs_queue.push_back({
+            callback,
+            args
+        });
+        pthread_mutex_unlock(&lock);
+    }
+    
+    void Queue::start(){
+        this->run_loop(this);
+    }
+    
+    void Queue::start_nonblocking(){
+        pthread_t main;
+        assert (pthread_create (&main, NULL, this->run_loop, this) == 0);
+    }
+}
